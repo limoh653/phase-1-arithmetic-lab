51e012eb569062338296a5b8ef47ff301d922472
limoh653 <limohesbon7@gmail.com>
2024-06-26 20:45:34 +0300

    functions

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Write your code he
+const num1=2
+const num2=31
+const multiply=num1*num2
+multiply
+const random=Math.floor(Math.random() * 10) +1
+random
+const num3=9
+const num4=5
+const mod = num3%num4
+mod
+const max=20
+max
+
